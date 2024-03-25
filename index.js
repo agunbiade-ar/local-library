39f3f05d0dc818e5120796298fc8ff0fa21feb82
Agunbiade Abdulrasheed <79267850+agunbiade-ar@users.noreply.github.com>
2024-03-25 18:16:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const port = process.env.port;
+const db_username = process.env.db_username;
+const db_password = process.env.db_password;
+const db = process.env.db;
+const app = express();
+
+function startApp() {
+  mongoose
+    .connect(
+      `mongodb+srv://${db_username}:${db_password}@cluster0.fd0xjdv.mongodb.net/${db}?retryWrites=true&w=majority&appName=Cluster0`
+    )
+    .then(() => console.log('Connected to mongodb atlas!'))
+    .catch((e) => {
+      console.log('an error occured while trying to connect to database ' + e);
+    })
+    .then(() => {
+      app.listen(port, () => {
+        console.log('app listening on port ' + port);
+      });
+    })
+    .catch((e) => {
+      console.log('could not connect to server ' + e);
+    });
+}
+
+startApp();
+
+app.get('/', (req, res) => {
+  res.send('hello there Zahir');
+});
